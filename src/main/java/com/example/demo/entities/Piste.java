c409fdd9ae67ff163061e8cb0f1c51ad0fdb7c41
achraf <achraf.saoud@etudiant-fst.utm.tn>
2025-02-19 11:41:29 +0100

    map the piste and skiers

--- src/main/java/com/example/demo/entities/Piste.java
+++ src/main/java/com/example/demo/entities/Piste.java
@@ -1,8 +1,11 @@
 package com.example.demo.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,4 +26,7 @@ public class Piste {
     private Integer length;
 
     private Integer slope;
+
+    @ManyToMany
+    private List<Skier> skiers;
 }
--- src/main/java/com/example/demo/entities/Skier.java
+++ src/main/java/com/example/demo/entities/Skier.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -38,4 +39,8 @@ public class Skier {
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "subscription_id")
     private Subscription subscription;
+
+
+    @ManyToMany
+    private List<Piste> pistes;
 }
